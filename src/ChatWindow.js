168e5f530b904227554b7b948a6852901682d50c
ayan4m1 <andrew@bulletlogic.com>
2024-11-30 11:36:49 -0500

    fix string interpolation of chat api url

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -66,7 +66,7 @@ export default function ChatWindow({ show, onDismiss }) {
 
     const fetchData = async () => {
       const response = await fetch(
-        `http://${BCF_CHAT_API_URL}/answer?question=${encodeURIComponent(question)}`
+        `${BCF_CHAT_API_URL}/answer?question=${encodeURIComponent(question)}`
       );
       const answer = await response.text();
 
