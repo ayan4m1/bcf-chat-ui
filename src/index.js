c9abbbd8bb4d995e498e040367274229748e59ee
ayan4m1 <andrew@bulletlogic.com>
2024-11-30 11:39:17 -0500

    guard against chat-widget not existing on page

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import './index.scss';
 // eslint-disable-next-line no-unused-vars
 import App from './App.js';
 
-createRoot(document.getElementById('chat-widget')).render(<App />);
+const widget = document.getElementById('chat-widget');
+
+if (widget) {
+  createRoot(widget).render(<App />);
+}
